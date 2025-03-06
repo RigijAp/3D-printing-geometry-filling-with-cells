6e0aefcb845cfff7df13429184b7ef308843de7e
Anna Step <77585945+RigijAp@users.noreply.github.com>
2025-03-06 10:32:34 +0800

    Update model_filling.m

--- model_filling.m
+++ model_filling.m
@@ -3,24 +3,23 @@ function out = model(alpha, k)
 %----------------------------------------------------
 %-----------------------parameters-------------------
 %----------------------------------------------------
-a = 10;
+% a unit size to recover the geometry
+a = 10;    
 
+% overall sizes of the geometry
 h = 3;
 l = 4;
 m = 6;
 
-alpha = 3618;
 
-k = 4;
-
+%----------------------------------------------------
+%-----------------------run-COMSOL-------------------
+%----------------------------------------------------
 
 import com.comsol.model.*
 import com.comsol.model.util.*
-
 model = ModelUtil.create('model');
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%-CHANGE!!!!!!!-%%%%%%%%%%%%%%%%%%%%%%%%%%
-model.modelPath('E:\MLa\2025_03_stl-stp\'); % CHANGE
+model.modelPath('E:\'); % CHANGE
 
 model.component.create('comp1', true);
 
@@ -35,16 +34,16 @@ model.component('comp1').geom('geom1').selection.create('csel1', 'CumulativeSele
 model.component('comp1').geom('geom1').selection('csel1').label('singlecell');
 
 %-----------------reading-the-single-cell-topology---------------------------
-
-
 ind = 1;
 for x = 0:a:l*a
     for y = 0:a:m*a
         for z = 0:a:h*a
             model.component('comp1').geom('geom1').create("imp"+ind, 'Import');
-            model.component('comp1').geom('geom1').feature("imp"+ind).set('filename', "E:\MLa\2025_03_stl-stp\inp\"+alpha+".stp");
+            model.component('comp1').geom('geom1').feature("imp"+ind).set('filename', "E:\ <...> .stp");
             model.component('comp1').geom('geom1').feature("imp"+ind).set('selresult', true);
            
+            % optional!%
+            
             model.component('comp1').geom('geom1').create("mov"+ind, 'Move');
             model.component('comp1').geom('geom1').feature("mov"+ind).set('displx', x + a/2);
             model.component('comp1').geom('geom1').feature("mov"+ind).set('disply', y + a/2);
@@ -62,15 +61,16 @@ model.component('comp1').geom('geom1').create('struct', 'Union');
 model.component('comp1').geom('geom1').feature('struct').selection('input').named('csel1');
 model.component('comp1').geom('geom1').run('struct');
 
-%model.component('comp1').geom('geom1').create('fill_geom', 'Import');
-%model.component('comp1').geom('geom1').feature('fill_geom').set('filename', "E:\MLa\2025_02_city_test\inp\"+geom+".stp");
-%model.component('comp1').geom('geom1').feature('fill_geom').set('selresult', true);
-%model.component('comp1').geom('geom1').feature('fill_geom').set('contributeto', 'csel4');
+model.component('comp1').geom('geom1').create('fill_geom', 'Import');
+model.component('comp1').geom('geom1').feature('fill_geom').set('filename', "E:\MLa\2025_02_city_test\inp\"+geom+".stp");
+model.component('comp1').geom('geom1').feature('fill_geom').set('selresult', true);
+model.component('comp1').geom('geom1').feature('fill_geom').set('contributeto', 'csel4');
 
-model.component('comp1').geom('geom1').create('fill_geom', 'Block');
-model.component('comp1').geom('geom1').feature('fill_geom').set('size', [l*a m*a h*a]);
-model.component('comp1').geom('geom1').feature('fill_geom').set('base', 'corner');
-model.component('comp1').geom('geom1').feature('fill_geom').set('pos', [0 0 0]);
+% -------test for filling a block-------
+%model.component('comp1').geom('geom1').create('fill_geom', 'Block');
+%model.component('comp1').geom('geom1').feature('fill_geom').set('size', [l*a m*a h*a]);
+%model.component('comp1').geom('geom1').feature('fill_geom').set('base', 'corner');
+%model.component('comp1').geom('geom1').feature('fill_geom').set('pos', [0 0 0]);
 
 model.component('comp1').geom('geom1').create('final_filling', 'Intersection');
 model.component('comp1').geom('geom1').feature('final_filling').selection('input').set({'fill_geom' 'struct'});
@@ -78,14 +78,13 @@ model.component('comp1').geom('geom1').run('final_filling');
 
 model.component('comp1').geom('geom1').run;
 
-mphsave(model,"E:\MLa\2025_03_stl-stp\test_fil_"+alpha);
+mphsave(model,"E: <...> test_fill@");
 
 model.component('comp1').geom('geom1').export.setType('stlbin');
-model.component('comp1').geom('geom1').export('E:\MLa\2025_03_stl-stp\out\geo.stl');
+model.component('comp1').geom('geom1').export('E: <...> .stl');
 
 model.component('comp1').geom('geom1').export.setType('step');
-model.component('comp1').geom('geom1').export('E:\MLa\2025_03_stl-stp\out\Untitled.step');
+model.component('comp1').geom('geom1').export('E: <...> .step');
 
-mphsave(model,"E:\MLa\2025_03_stl-stp\test_fil_"+alpha);
 out = model;
 
